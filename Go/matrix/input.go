5352ff93043498aef2762928e411d404dabeb5e6
magnon <magnong000@gmail.com>
2023-01-20 16:09:22 +0100

    input maj

--- Go/matrix/input.go
+++ Go/matrix/input.go
@@ -2,28 +2,44 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func input(oui string){
-	fmt.Println(oui)
+const (
+	matriceA = "a.txt"
+	matriceB = "b.txt"
+)
+
+func input() {
+	contentA, err := os.ReadFile(matriceA)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("A =\n", string(contentA))
+	contentB, err := os.ReadFile(matriceB)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("B =\n", string(contentB))
 
 	//blablabla
-
+	mA, nA := len(A), len(A[0])
+	nB, pB := len(B), len(B[0])
 	matriceA := make([][]float64, tailleAy)
 	matriceB := make([][]float64, tailleBy)
 
 	//matrice A
-	for y:= 0; y<tailleAy;y++ {
+	for y := 0; y < tailleAy; y++ {
 		ligne := make([]float64, tailleAx)
-		for x := 0; x<tailleAx; x++ {
+		for x := 0; x < tailleAx; x++ {
 			ligne[x] = float64(coefficientnumérique)
 		}
 	}
 
 	//matrice B
-	for y:= 0; y<tailleAy;y++ {
+	for y := 0; y < tailleAy; y++ {
 		ligne := make([]float64, tailleBx)
-		for x := 0; x<tailleBx; x++ {
+		for x := 0; x < tailleBx; x++ {
 			ligne[x] = float64(coefficientnumérique)
 		}
 	}
