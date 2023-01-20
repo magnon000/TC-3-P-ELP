2c68c4456ffba198b721b6bdad4e665b7ee0ff42
MONTEAGUDO Diego <d.monteagudo13@gmail.com>
2023-01-20 15:39:24 +0100

    début main et input

---
+++ Go/matrix/input.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func input(oui string){
+	fmt.Println(oui)
+}
---
+++ Go/matrix/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func multiplication(matriceA [][]float64, matriceB [][]float64){
+	fmt.Println(matriceA)
+}
+
+func main(){
+	fmt.Println("Test")
+	input("test2")
+	
+}
