b688e6f4d46845826934d3baf1f38d947fed0970
MONTEAGUDO Diego <d.monteagudo13@gmail.com>
2023-01-20 15:46:13 +0100

    début input.go

--- Go/matrix/input.go
+++ Go/matrix/input.go
@@ -6,4 +6,25 @@ import (
 
 func input(oui string){
 	fmt.Println(oui)
+
+	//blablabla
+
+	matriceA := make([][]float64, tailleAy)
+	matriceB := make([][]float64, tailleBy)
+
+	//matrice A
+	for y:= 0; y<tailleAy;y++ {
+		ligne := make([]float64, tailleAx)
+		for x := 0; x<tailleAx; x++ {
+			ligne[x] = float64(coefficientnumérique)
+		}
+	}
+
+	//matrice B
+	for y:= 0; y<tailleAy;y++ {
+		ligne := make([]float64, tailleBx)
+		for x := 0; x<tailleBx; x++ {
+			ligne[x] = float64(coefficientnumérique)
+		}
+	}
 }
