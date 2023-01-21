447123a3336d2527e8cd8361ace63a5494128762
MONTEAGUDO Diego <d.monteagudo13@gmail.com>
2023-01-22 00:15:49 +0100

    Passage aux go routines (problème: deadlock)

--- Go/matrix/main.go
+++ Go/matrix/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-func multiplication(matriceA [][]float64, matriceB [][]float64) [][]float64{
+type portionTodo struct {
+	positionY int
+	matriceB *[][]float64 //on a besoin de toutes les colonnes de B pour calculer 1 ligne de la mat produit
+	ligneYmatriceA *[]float64 //sera parcourue à chaque colonne X de B
+}
+
+type portionFinished struct {
+	positionY int
+	ligneFinale *[]float64 //la ligne Y remplie de la matrice produit
+}
+
+func multiplicationTotale(matriceA [][]float64, matriceB [][]float64) [][]float64{
 	//récupérer tailles (vérifié dans input : tailleAx==tailleBy)
 	tailleAy := len(matriceA)
 	tailleBx := len(matriceB[0])
@@ -25,6 +38,60 @@ func multiplication(matriceA [][]float64, matriceB [][]float64) [][]float64{
 
 	return matriceC
 
+}
+
+//calcule 1 ligne de la matrice produit
+func workerMultiplicationPartielle(wg *sync.WaitGroup, jobs chan *portionTodo, result chan *portionFinished){
+	//récupération des infos du channel
+	job :=<- jobs
+	tailleRecurrente := len(*job.ligneYmatriceA)
+	nombreColonnes := len((*job.matriceB)[0])
+
+	//partie calcul
+	ligne := make([]float64, nombreColonnes)
+	for x:=0; x<nombreColonnes; x++{
+		ligne[x] = 0
+		for i:=0; x<tailleRecurrente; i++ {
+			ligne[i] += (*job.ligneYmatriceA)[i] * (*job.matriceB)[i][x]
+		}
+	}
+
+	//rendu du travail effectué
+	rendu := portionFinished{positionY: job.positionY}
+	rendu.ligneFinale = &ligne
+	result <- &rendu
+	wg.Done()
+}
+
+func managerMultiplicationPartielle(matriceA [][]float64, matriceB [][]float64) [][]float64{
+	nombreWorkers := len(matriceA)
+	matriceC := make([][]float64, nombreWorkers)
+
+	var wg sync.WaitGroup
+	var jobsChannel chan *portionTodo
+	var resultChannel chan *portionFinished
+
+	for i:=0; i<nombreWorkers; i++ {
+		wg.Add(1)
+		go workerMultiplicationPartielle(&wg,jobsChannel,resultChannel)
+	}
+
+	for i:=0; i<nombreWorkers; i++ {
+		var portion portionTodo
+		portion = portionTodo{positionY: i, matriceB: &matriceB, ligneYmatriceA: &(matriceA[i])}
+		jobsChannel <- &portion
+		fmt.Println("tAAAAAAAAAAA")
+	}
+
+	wg.Wait()
+
+	for i:=0; i<nombreWorkers; i++ {
+		resultat :=<- resultChannel
+		pos := resultat.positionY
+		matriceC[pos] = *resultat.ligneFinale
+	}
+
+	return matriceC
 }
 
 func main(){
@@ -65,6 +132,17 @@ func main(){
 		matriceB[y] = ligne
 	}
 
-	fmt.Println(multiplication(matriceA, matriceB))
+	//test 1 thread
+	start1 := time.Now()
+	fmt.Println(multiplicationTotale(matriceA, matriceB))
+	end1 := time.Now()
+	fmt.Println("Durée du calcul 1 thread :",end1.Sub(start1))
+
+	//test parallélisme
+	start2 := time.Now()
+	fmt.Println(managerMultiplicationPartielle(matriceA, matriceB))
+	end2 := time.Now()
+	fmt.Println("Durée du calcul 1 thread :",end2.Sub(start2))
+
 
 }
